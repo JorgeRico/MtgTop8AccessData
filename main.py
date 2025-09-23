ab500f6694d37b9c8b34b0cc767dc6535e7e367a
JorgeRico <jorge.rico.garriga@gmail.com>
2025-09-23 21:07:28 +0200

    main original flux

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from classes.menu import Menu
 if __name__ == "__main__":
     menu = Menu()
     # can be commented if exists excel and json data files
-    # menu.accessToExcel()
-    # # can be commented if exists excel and json data files
-    # menu.excelToJson()
+    menu.accessToExcel()
+    # can be commented if exists excel and json data files
+    menu.excelToJson()
     # insert data on mysql DB
     menu.jsonToDatabase()
 
